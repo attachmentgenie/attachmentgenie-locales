185660cd8b2f97b43a81d3a54763dc9acc172f22
Jan-Otto Kröpke <kroepke@outlook.com>
2017-02-21 18:57:46 +0100

    Use newer stdlib datatypes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 class locales (
-  Array[String]                 $available      = $::locales::params::available,
-  String                        $default_value  = $::locales::params::default_value,
-  Stdlib::Compat::Absolute_path $localegenfile  = $::locales::params::localegenfile,
-  String                        $package_ensure = $::locales::params::package_ensure,
-  String                        $package_name   = $::locales::params::package_name,
+  Array[String]        $available      = $::locales::params::available,
+  String               $default_value  = $::locales::params::default_value,
+  Stdlib::Absolutepath $localegenfile  = $::locales::params::localegenfile,
+  String               $package_ensure = $::locales::params::package_ensure,
+  String               $package_name   = $::locales::params::package_name,
 ) inherits locales::params {
 
   anchor { 'locales::begin': }  ->
